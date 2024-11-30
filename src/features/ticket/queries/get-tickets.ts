6258b9c1836539ff7480beba9867cde952b66867
Robert Crocker <rcrocker13@gmail.com>
2024-11-29 20:50:43 -0600

    Added error boundary with the error.tsx file.

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -5,6 +5,8 @@ import { type Ticket } from "../types";
 export const getTickets = async (): Promise<Ticket[]> => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
+  throw new Error("Failed to fetch tickets");
+
   return new Promise((resolve) => {
     resolve(initialTickets);
   });
