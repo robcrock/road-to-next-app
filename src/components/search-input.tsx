7dad797ab4e9ce34ed2028601c6bdf53777eecdf
Robert Crocker <rcrocker13@gmail.com>
2024-12-21 16:23:46 -0600

    Fixed the search input.

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import { Input } from "./ui/input";
 
