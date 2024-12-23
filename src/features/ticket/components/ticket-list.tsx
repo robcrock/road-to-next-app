8cf78f0efde65553be917fb279f7f63d66ad6de3
Robert Crocker <rcrocker13@gmail.com>
2024-12-22 22:26:36 -0600

    Implimented nuqs.

--- src/features/ticket/components/ticket-list.tsx
+++ src/features/ticket/components/ticket-list.tsx
@@ -21,7 +21,6 @@ const TicketList = async ({ userId, searchParams }: TicketListProps) => {
         <div className="flex gap-x-2">
           <SearchInput placeholder="Search tickets...." />
           <SortSelect
-            defaultValue={"newest"}
             options={[
               { label: "Newest", value: "newest" },
               { label: "Bounty", value: "bounty" },
--- src/features/ticket/search-params.ts
+++ src/features/ticket/search-params.ts
@@ -1,8 +1,19 @@
 import { createSearchParamsCache, parseAsString } from "nuqs/server";
 
+export const searchParser = parseAsString.withDefault("").withOptions({
+  // we set it to false when we want to requery our server
+  shallow: false,
+  clearOnDefault: true,
+});
+export const sortParser = parseAsString.withDefault("newest").withOptions({
+  // we set it to false when we want to requery our server
+  shallow: false,
+  clearOnDefault: true,
+});
+
 export const searchParamsCache = createSearchParamsCache({
-  search: parseAsString.withDefault(""),
-  sort: parseAsString.withDefault("newest"),
+  search: searchParser,
+  sort: sortParser,
 });
 
 export type ParsedSearchParams = Awaited<
