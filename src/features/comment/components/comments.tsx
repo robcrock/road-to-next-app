35341ac4489b58c6dc49e6ac94cc8cc97dcc82ba
Robert Crocker <rcrocker13@gmail.com>
2024-12-25 23:24:26 -0600

    Able to create comments.

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,5 +1,8 @@
+import { CardCompact } from "@/components/card-compact";
+
 import { getComments } from "../queries/get-comments";
 
+import { CommentCreateForm } from "./comment-create-form";
 import { CommentItem } from "./comment-item";
 
 type CommentsProps = {
@@ -10,11 +13,18 @@ const Comments = async ({ ticketId }: CommentsProps) => {
   const comments = await getComments(ticketId);
 
   return (
-    <div className="flex flex-col gap-y-2 ml-8">
-      {comments.map((comment) => {
-        return <CommentItem key={comment.id} comment={comment} />;
-      })}
-    </div>
+    <>
+      <CardCompact
+        title={"Create Comment"}
+        description={"A new comment will be created"}
+        content={<CommentCreateForm ticketId={ticketId} />}
+      />
+      <div className="flex flex-col gap-y-2 ml-8">
+        {comments.map((comment) => {
+          return <CommentItem key={comment.id} comment={comment} />;
+        })}
+      </div>
+    </>
   );
 };
 
